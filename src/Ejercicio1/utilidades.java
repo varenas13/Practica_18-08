bdd8c6bfb8572024646cd02fa1784547e238d01a
Valentina <vale@gmail.com>
2026-08-18 20:05:39 -0400

    Creacion de metodos utilidades imprimir

---
+++ src/Ejercicio1/utilidades.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio1;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class utilidades {
+    public void imprimir(int []num){
+        for (int i = 0; i < num.length; i++) {
+            System.out.println("EL valor "+(i+1)+"es: "+num[i]);
+        }
+    }
+   
+}
